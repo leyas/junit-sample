78197502fda99d5eb88f220892967207c3589ebb
Szymon Leyk <leyas@wp.pl>
2020-06-10 10:13:18 +0200

    repeatedTest

--- src/main/java/pl/szymonleyk/junit/Calculator.java
+++ src/main/java/pl/szymonleyk/junit/Calculator.java
@@ -7,4 +7,8 @@ public final class Calculator {
 	public static int add(int ...numbers) {
 		return Arrays.stream(numbers).reduce(0, (a, b)->a+b);
 	}
+	
+	public static double random() {
+		return Math.random();
+	}
 }
--- src/test/java/pl/szymonleyk/junit/CalculatorTest.java
+++ src/test/java/pl/szymonleyk/junit/CalculatorTest.java
@@ -2,10 +2,12 @@ package pl.szymonleyk.junit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
-
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static pl.szymonleyk.junit.Calculator.add;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -38,5 +40,15 @@ public class CalculatorTest {
 	@CsvSource({"1, 0, 1", "23,32,55", "0,22,22"})
 	void addEqualsTest(int first, int second, int expected) {
 		assertEquals(expected, add(first, second));
+	}
+	
+	@RepeatedTest(value = 5)
+	void randomTest() {
+		assertTrue(Calculator.random() > 0);
+	}
+	
+	@RepeatedTest(5)
+	void randomTest2() {
+		assertNotNull(Calculator.random());
 	}
 }
